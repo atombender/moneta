139ba9dc46e759225961824a1fb188dd369c9540
Alexander Staubo <alex@bengler.no>
2009-05-21 01:38:37 +0200

    Extended Moneta::File with a customizable path mapping mechanism.

--- lib/moneta/file.rb
+++ lib/moneta/file.rb
@@ -8,9 +8,77 @@ require "fileutils"
 
 module Moneta
   class File
+
+    # Abstract base class for path mappers. Custom mappers can plug in their
+    # own mapping of keys to file names.
+    class AbstractMapper
+
+      # Initialize mapper with a root directory.
+      def initialize(root)
+        @root = root
+        if ::File.file?(@root)
+          raise StandardError, "The path you supplied #{@root} is a file"
+        elsif !::File.exists?(@root)
+          FileUtils.mkdir_p(@root)
+        end
+      end
+
+      # Clear the root directory's contents.
+      def clear
+        FileUtils.rm_rf(@root)
+        FileUtils.mkdir(@root)
+      end
+
+      attr_reader :root
+    end
+
+    # A simple mapper which maps keys directly to file names. Eg., the key
+    # +foo+ will be mapped to +/root/foo+.
+    class SimpleMapper < AbstractMapper
+      def path_for(key)
+        return ::File.join(@root, key)
+      end
+    end
+
+    # A hashed mapper which can be configured to split the key across multiple
+    # directory levels in order to optimize file-system access. The default
+    # nesting level is 2. For example, given a nesting level of 2, the key +foo+
+    # will be mapped to +/root/f6/39/foo+; given a nesting level of 3, it will
+    # be mapped to +/root/f6/39/e3+.
+    class HashDistributedMapper < AbstractMapper
+      # Maximum number of nesting levels
+      MAX_LEVELS = 4
+
+      # Initialize mapper. Options:
+      #
+      # * +:path+ - root directory.
+      # * +:levels+ - number of levels (defaults to 2).
+      #
+      def initialize(options)
+        super(options[:path])
+        @levels = options[:levels] || 2
+        if @levels > 16 / SPLIT_PER_LEVEL
+          raise ArgumentError, "Maximum number of levels is #{16 / SPLIT_PER_LEVEL}"
+        end
+        @padding = @levels * SPLIT_PER_LEVEL
+        @splits = (0..(@levels - 1)).to_a.map { |n|
+          [n * SPLIT_PER_LEVEL, SPLIT_PER_LEVEL] }
+      end
+
+      def path_for(key)
+        hash = key.hash.to_s(16).reverse.ljust(@padding, "0")
+        parents = ::File.join(@root, @splits.map { |split| hash[*split] })
+        FileUtils.mkdir_p(parents)
+        ::File.join(parents, key)
+      end
+
+      private
+        SPLIT_PER_LEVEL = 2
+    end
+
     class Expiration
-      def initialize(directory)
-        @directory = directory
+      def initialize(mapper)
+        @mapper = mapper
       end
       
       def [](key)
@@ -31,36 +99,31 @@ module Moneta
 
       private
       def xattr(key)
-        ::Xattr.new(::File.join(@directory, key))
+        ::Xattr.new(@mapper.path_for(key))
       end
     end
     
     def initialize(options = {})
-      @directory = options[:path]
-      if ::File.file?(@directory)
-        raise StandardError, "The path you supplied #{@directory} is a file"
-      elsif !::File.exists?(@directory)
-        FileUtils.mkdir_p(@directory)
-      end
-      
-      @expiration = Expiration.new(@directory)
+      @mapper = options[:mapper] || SimpleMapper.new(options[:path])
+      @expiration = Expiration.new(@mapper)
     end
     
     module Implementation
       def key?(key)
-        ::File.exist?(path(key))
+        ::File.exist?(@mapper.path_for(key))
       end
       
       alias has_key? key?
       
       def [](key)
-        if ::File.exist?(path(key))
-          Marshal.load(::File.read(path(key)))
+        path = @mapper.path_for(key)
+        if ::File.exist?(path)
+          Marshal.load(::File.read(path))
         end
       end
       
       def []=(key, value)
-        ::File.open(path(key), "w") do |file|
+        ::File.open(@mapper.path_for(key), "w") do |file|
           contents = Marshal.dump(value)
           file.puts(contents)
         end
@@ -68,19 +131,13 @@ module Moneta
             
       def delete(key)
         value = self[key]
-        FileUtils.rm(path(key))
+        FileUtils.rm(@mapper.path_for(key))
         value
       rescue Errno::ENOENT
       end
             
       def clear
-        FileUtils.rm_rf(@directory)
-        FileUtils.mkdir(@directory)
-      end
-      
-      private
-      def path(key)
-        ::File.join(@directory, key)
+        @mapper.clear
       end
     end
     include Implementation
--- spec/moneta_file_spec.rb
+++ spec/moneta_file_spec.rb
@@ -3,9 +3,26 @@ require File.dirname(__FILE__) + '/spec_helper'
 begin
   require "moneta/file"
 
-  describe "Moneta::File" do
+  describe "Moneta::File with SimpleMapper" do
     before(:each) do
       @cache = Moneta::File.new(:path => File.join(File.dirname(__FILE__), "file_cache"))
+      @cache.clear
+    end
+
+    after(:all) do
+      FileUtils.rm_rf(File.join(File.dirname(__FILE__), "file_cache"))
+    end
+
+    if ENV['MONETA_TEST'].nil? || ENV['MONETA_TEST'] == 'file'
+      it_should_behave_like "a read/write Moneta cache"
+    end
+  end
+
+  describe "Moneta::File with HashDistributedMapper" do
+    before(:each) do
+      @cache = Moneta::File.new(:mapper => Moneta::File::HashDistributedMapper.new(
+        :path => File.join(File.dirname(__FILE__), "file_cache"),
+        :levels => 2))
       @cache.clear
     end
 
